b1615a920c3e3649d54b3f345ae5a5111e0efad9
kusakina0608 <platina.kusakina@gmail.com>
2022-03-15 22:54:16 +0900

    [ex3] basic.html 레이아웃 추가

--- ex3/src/main/java/com/kina/ex3/controller/SampleController.java
+++ ex3/src/main/java/com/kina/ex3/controller/SampleController.java
@@ -66,7 +66,7 @@ public class SampleController {
 
     }
 
-    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate"})
+    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate", "/exSidebar"})
     public void exLayout1() {
 
         log.info("exLayout....................");
