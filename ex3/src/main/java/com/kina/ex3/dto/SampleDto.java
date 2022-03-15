8f9ab61a8c8840cacef22d88a4cb0a54b8cb715d
kusakina0608 <platina.kusakina@gmail.com>
2022-03-15 21:49:43 +0900

    [ex3] 프로젝트 생성

---
+++ ex3/src/main/java/com/kina/ex3/dto/SampleDto.java
@@ -0,0 +1,20 @@
+package com.kina.ex3.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDto {
+
+    private Long sno;
+
+    private String first;
+
+    private String last;
+
+    private LocalDateTime regTime;
+
+}
