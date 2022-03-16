e6545f785db8c707c332d0f71fabf794ef9454a7
kusakina0608 <platina.kusakina@gmail.com>
2022-03-16 09:43:41 +0900

    [guestbook] Entity, Repository 추가

---
+++ guestbook/src/main/java/com/kina/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,7 @@
+package com.kina.guestbook.repository;
+
+import com.kina.guestbook.entity.Guestbook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+}
