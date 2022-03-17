b701f72a53cd80c468c2bdd2b7eca45fa857715d
kusakina0608 <platina.kusakina@gmail.com>
2022-03-17 23:58:51 +0900

    [guestbook] GuestbookRepository에 QuerydslPredicateExcutor 인터페이스 추가

--- guestbook/src/main/java/com/kina/guestbook/repository/GuestbookRepository.java
+++ guestbook/src/main/java/com/kina/guestbook/repository/GuestbookRepository.java
@@ -2,6 +2,9 @@ package com.kina.guestbook.repository;
 
 import com.kina.guestbook.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+public interface GuestbookRepository extends
+        JpaRepository<Guestbook, Long>,
+        QuerydslPredicateExecutor<Guestbook> {
 }
